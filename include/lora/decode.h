e97a70dea1324fd0b243bec6a7628b0fab11ca66
matt-knight <matt@bastille.io>
2016-06-14 16:44:58 -0400

    Demod and decode block templates added.

---
+++ include/lora/decode.h
@@ -0,0 +1,56 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2016 <+YOU OR YOUR COMPANY+>.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+
+#ifndef INCLUDED_LORA_DECODE_H
+#define INCLUDED_LORA_DECODE_H
+
+#include <lora/api.h>
+#include <gnuradio/block.h>
+
+namespace gr {
+  namespace lora {
+
+    /*!
+     * \brief <+description of block+>
+     * \ingroup lora
+     *
+     */
+    class LORA_API decode : virtual public gr::block
+    {
+     public:
+      typedef boost::shared_ptr<decode> sptr;
+
+      /*!
+       * \brief Return a shared_ptr to a new instance of lora::decode.
+       *
+       * To avoid accidental use of raw pointers, lora::decode's
+       * constructor is in a private implementation
+       * class. lora::decode::make is the public interface for
+       * creating new instances.
+       */
+      static sptr make();
+    };
+
+  } // namespace lora
+} // namespace gr
+
+#endif /* INCLUDED_LORA_DECODE_H */
+
--- swig/lora_swig.i
+++ swig/lora_swig.i
@@ -8,6 +8,12 @@
 %include "lora_swig_doc.i"
 
 %{
+#include "lora/demod.h"
+#include "lora/decode.h"
 %}
 
 
+%include "lora/demod.h"
+GR_SWIG_BLOCK_MAGIC2(lora, demod);
+%include "lora/decode.h"
+GR_SWIG_BLOCK_MAGIC2(lora, decode);
